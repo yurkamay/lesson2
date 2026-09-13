6c421802dc730d3bdb2481f6ac3bf0477a907024
yurkamay <yurkamay2012@gmail.com>
2026-09-13 15:28:19 +0300

    firsttask

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ print(car1)'''
    attribute_list = [f'{key}="{value}"' for key, value in attributes.items()]
    attribute_string = " " + " ".join(attribute_list) if attribute_list else ""
    return f'<{tag}{attribute_string}>{content}</{tag}>'''''
-class item():
+'''class item():
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
@@ -45,8 +45,92 @@ it2 = item("Orange", 200)
 wh1.add_item(it1,it2)
 wh1.total_weight()
 print(wh1.inventory)
+class Employee():
+    company = "Techcor"
+    def work(self):
+        print("I am working")
+class Developer(Employee):
+    language = "Python"
+    def work(self):
+        print("I write code")
+dev = Developer()
+print(dev.company)
+dev.work()'''
+'''class Character():
+    def __init__(self, health = 100):
+        self.health = health
+    def attack(self):
+        print("Базова атака")
+class Warrior(Character):
+    def __init__(self, health = 150):
+        self.health = health
+class mage(Character):
+    def __init__(self, health = 100):
 
+        self.health = health
 
+    def attack(self):
+        print("атака магією")
 
+c1 = Character()
+w1 = Warrior()
+m1 = mage()
+print(c1.health)
+print(w1.health)
+print(m1.health)
+m1.attack()
+w1.attack()'''
+'''class Account():
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+    def get_balance(self):
+        return self.__balance
+    def deposite(self,ammount):
+        if ammount > 0:
+            self.__balance += ammount
+        else:
+            print("Cevf vf' ,enb ,skmij. pf 0")
+acc = Account(balance=1000)
+print(acc.get_balance())
+acc.deposite(100)
+print(acc.get_balance())'''
+'''class user():
+    def __init__(self, name, password):
+        self.name = name
+        self.__password = password
+    def check_password(self, password):
+        password1 = int(input("Please enter your password: "))
 
-
+        if password1 == self.__password:
+            print("True")
+        else:
+            print("False")
+u1 = user("John", 1234)
+u1.check_password("")'''
+'''class Shape():
+    def __init__(self, color):
+        self.color = color
+class rectangle(Shape):
+    def __init__(self, color ,width, height):
+        super().__init__(color)
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+s1 = Shape("red")
+r1 = rectangle(color = "blue", width = 100, height = 100)
+print(r1.area())'''
+class Document():
+    pass
+class Image():
+    pass
+d1 = Document()
+i1 = Image()
+d2 = Document()
+files = ( d1, i1, d2, 42)
+for item in files:
+    if isinstance(item,Document):
+        print("це документ")
+    else :
+        print("Не документ")
